9b443636bfcab457ef71c3b1e32e0d83837b6033
Sofia Soerensen <sofia191004@gmail.com>
2024-12-16 18:12:57 +0100

    Improved user experience

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function displayMealIdea(response) {
-  console.log("meal idea generated");
   new Typewriter("#meal", {
     strings: response.data.answer,
     autoStart: true,
@@ -15,12 +14,12 @@ function generateMealIdea(event) {
   let apiKey = "2008d7d14b59443btfdod2382a33e32f";
   let prompt = `Generate a meal idea centered on ${instructionsInput.value}`;
   let context =
-    "You are a skilled cook with knowledge of many recipes, you provide the recipe for your recommended meal, with a text of instructions and bulletpointed list of ingredients. Use HTML. Exlude your intro to the dish.";
+    "You are a skilled cook with knowledge of many recipes, you provide the recipe for your recommended meal, with a text of instructions and bulletpointed list of ingredients. Use HTML. Use metric system. Please exlude your intro to the dish!";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("generating meal idea");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let mealElement = document.querySelector("#meal");
+  mealElement.classList.remove("hidden");
+  mealElement.innerHTML = `<div class="generating">⏳ Generating a meal idea with ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayMealIdea);
 }
